5f098c3287b4641782c8a6aea60c46154d0b252a
Ricardo Arguello <ricardo.args@outlook.com>
2022-01-28 11:43:02 -0600

    creación de base de datos y entidades

---
+++ app/src/main/java/com/example/mynotesjava/data/NoteRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.example.mynotesjava.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.mynotesjava.interfaces.NoteDao;
+
+@Database(entities = {NoteEntity.class}, version = 1)
+public abstract class NoteRoomDatabase extends RoomDatabase {
+    public abstract NoteDao noteDao();
+    private static volatile NoteRoomDatabase INSTANCE;
+
+    public static NoteRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (NoteRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteRoomDatabase.class, "notes_database")
+                    .build();
+                }
+            }
+        }
+        return INSTANCE
+    }
+}
---
+++ app/src/main/java/com/example/mynotesjava/interfaces/NoteDao.java
@@ -0,0 +1,30 @@
+package com.example.mynotesjava.interfaces;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.example.mynotesjava.data.NoteEntity;
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(NoteEntity noteEntity);
+
+    @Update
+    void update(NoteEntity noteEntity);
+
+    @Query("DELETE FROM notes")
+    void deleteAll();
+
+    @Query("DELETE FROM notes WHERE id = :idNote")
+    void deleteById(int idNote);
+
+    @Query("SELECT * FROM notes ORDER BY title ASC")
+    LiveData<List<NoteEntity>> getAll();
+
+    @Query("SELECT * FROM notes WHERE favorite LIKE 'true'")
+    LiveData<List<NoteEntity>> getAllFavorites();
+}
